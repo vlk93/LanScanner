f5e1648635d44ddecd82f519b262ccbb437957f9
Kacper Goławski <kacpergolawski@gmail.com>
2017-11-30 18:20:20 +0100

    Initial commit via Visual Studio

--- LanScanner/Program.cs
+++ LanScanner/Program.cs
@@ -17,6 +17,9 @@ namespace LanScanner
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            Console.WriteLine("Test");
+
         }
     }
 }
