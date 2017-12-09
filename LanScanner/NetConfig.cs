62853d8408457638c3d70805736a75048a2cc54f
Kacper Goławski <kacpergolawski@gmail.com>
2017-12-09 16:31:06 +0100

    Dodana klasa NetConfig. Do dodania pozostałe metody tej klasy.

---
+++ LanScanner/NetConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace LanScanner
+{
+    static class NetConfig
+    {
+        static string nazwaHosta = Dns.GetHostName();
+        static IPHostEntry adresyHosta = Dns.GetHostEntry(nazwaHosta); //trzeba przeszukać po wybranym interfejsie
+        static string ipHosta;
+        
+
+        static void pokazInterfejsy() //pobierze listę interfejsów
+        {
+            NetworkInterface[] interfejsy = NetworkInterface.GetAllNetworkInterfaces();
+            string listaInterfejsy = interfejsy.ToString();
+            Console.WriteLine(listaInterfejsy);
+            listaInterfejsy.
+        }
+
+
+
+    }
+}
